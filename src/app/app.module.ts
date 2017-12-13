36797ee2f136a707a6a1a93b3c0d660a964066a9
Wagner Porto <wagnerporto@gmail.com>
2017-12-13 00:15:00 -0300

    Remove app.component from the library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatSidenavModule, MatToolbarModule } from '@angular/material';
 
-
-import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
     LayoutComponent
   ],
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule
   ],
@@ -23,6 +18,5 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class LayoutModule { }
